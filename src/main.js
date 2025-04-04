2137762b23c77f51c9871561781236c6694deca3
Débora Guimarães <debb.oliveira2@gmail.com>
2025-04-04 20:47:49 -0300

    add elements for querySelector

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+const container = document.querySelector("#container");
+
+const inputTask = document.querySelector("#input-task");
+const btnAddTask = document.querySelector("#btn-add-task");
+const btnCompleteTask = document.querySelector("#btn-complete-task");
+const btnDiscard = document.querySelector("#btn-discard");
+
+const taskData = [];
